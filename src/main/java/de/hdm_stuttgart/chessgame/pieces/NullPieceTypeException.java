238fbf88f843638d097c2e9e02871dfd0017948a
mw194 <the.saph.bot@gmail.com>
2017-04-29 20:50:18 +0200

    Even more JavaDoc

--- src/main/java/de/hdm_stuttgart/chessgame/pieces/NullPieceTypeException.java
+++ src/main/java/de/hdm_stuttgart/chessgame/pieces/NullPieceTypeException.java
@@ -1,5 +1,9 @@
 package de.hdm_stuttgart.chessgame.pieces;
 
+/**
+ * Utilized by the {@link de.hdm_stuttgart.chessgame.pieces.ChessPieceFactory}.
+ * Throws on invalid {@link de.hdm_stuttgart.chessgame.pieces.EnumPieceType}.
+ */
 public class NullPieceTypeException extends RuntimeException
 {
 	private static final long serialVersionUID = 1L;
