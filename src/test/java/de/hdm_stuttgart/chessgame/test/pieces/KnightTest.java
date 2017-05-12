a8f77b53a106b2cd772cdfacba75f8d34af1a570
da031 <da031@hdm-stuttgart.de>
2017-05-12 18:01:45 +0200

    Testklassen für Knight und Queen angelegt.

--- src/test/java/de/hdm_stuttgart/chessgame/test/pieces/KnightTest.java
+++ src/test/java/de/hdm_stuttgart/chessgame/test/pieces/KnightTest.java
@@ -3,7 +3,50 @@ package de.hdm_stuttgart.chessgame.test.pieces;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import de.hdm_stuttgart.chessgame.pieces.ChessPiece;
+import de.hdm_stuttgart.chessgame.pieces.ChessPieceFactory;
+import de.hdm_stuttgart.chessgame.pieces.EnumPieceColor;
+import de.hdm_stuttgart.chessgame.pieces.EnumPieceType;
+
 public class KnightTest
 {
-	// Implementier mich.
+	@Test
+	public void testValidMovement() { 
+		ChessPiece[][] board = new ChessPiece[8][8]; // Dummy Board 
+		// Fill with pieces 
+		ChessPiece dummyKnight = ChessPieceFactory.getInstance(EnumPieceColor.WHITE, 4, 4, EnumPieceType.KNIGHT); 
+		board[4][4] = dummyKnight; 
+		//board[4][2] = dummyKnight; 
+
+
+		assertTrue(dummyKnight.canMove(6, 5, board)); // rechts oben 
+		assertTrue(dummyKnight.canMove(6, 3, board)); // rechts unten 
+		assertTrue(dummyKnight.canMove(2, 5, board)); // links oben 
+		assertTrue(dummyKnight.canMove(2, 3, board)); // rechts unten 
+		assertTrue(dummyKnight.canMove(5, 6, board)); // oben rechts 
+		assertTrue(dummyKnight.canMove(3, 6, board)); // oben links 
+		assertTrue(dummyKnight.canMove(5, 2, board)); // unten rechts 
+		assertTrue(dummyKnight.canMove(3, 2, board)); // rechts oben 
+
+		}
+	
+	@Test
+	public void testInvalidMovement()
+	{
+		
+		ChessPiece[][] board = new ChessPiece[8][8]; // Dummy Board 
+		// Fill with pieces 
+		ChessPiece dummyKnight = ChessPieceFactory.getInstance(EnumPieceColor.WHITE, 2, 2, EnumPieceType.KNIGHT); 
+		
+		board[2][2] = dummyKnight;
+		board[4][3] = ChessPieceFactory.getInstance(EnumPieceColor.WHITE, 4, 3, EnumPieceType.KNIGHT); 
+		assertFalse(dummyKnight.canMove(7, 5, board));
+		assertFalse(dummyKnight.canMove(4, 3, board));
+		assertFalse(dummyKnight.canMove(2, 6, board));
+		
+		
+		
+	}
+	
+
 }
--- src/test/java/de/hdm_stuttgart/chessgame/test/pieces/QueenTest.java
+++ src/test/java/de/hdm_stuttgart/chessgame/test/pieces/QueenTest.java
@@ -3,7 +3,44 @@ package de.hdm_stuttgart.chessgame.test.pieces;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import de.hdm_stuttgart.chessgame.pieces.ChessPiece;
+import de.hdm_stuttgart.chessgame.pieces.ChessPieceFactory;
+import de.hdm_stuttgart.chessgame.pieces.EnumPieceColor;
+import de.hdm_stuttgart.chessgame.pieces.EnumPieceType;
+
 public class QueenTest
 {
 	// Implementier mich.
+
+	@Test
+	public void testValidMovement() { 
+
+		ChessPiece[][] board = new ChessPiece[8][8]; // Dummy Board 
+		// Fill with pieces 
+		ChessPiece dummyQueen = ChessPieceFactory.getInstance(EnumPieceColor.WHITE, 5, 5, EnumPieceType.QUEEN); 
+		board[5][5] = dummyQueen;
+
+
+		assertTrue(dummyQueen.canMove(7, 7, board));
+		assertTrue(dummyQueen.canMove(5, 7, board));
+		assertTrue(dummyQueen.canMove(5, 2, board));
+		assertTrue(dummyQueen.canMove(2, 2, board));
+
+	}
+	
+	@Test
+	public void testInvalidMovement() {
+		ChessPiece[][] board = new ChessPiece[8][8]; // Dummy Board 
+		// Fill with pieces 
+		ChessPiece dummyQueen = ChessPieceFactory.getInstance(EnumPieceColor.WHITE, 5, 5, EnumPieceType.QUEEN); 
+		board[5][5] = dummyQueen;
+		board[7][7] = ChessPieceFactory.getInstance(EnumPieceColor.WHITE, 7, 7, EnumPieceType.QUEEN); 
+
+		assertFalse(dummyQueen.canMove(7, 7, board));
+		assertFalse(dummyQueen.canMove(7, 4, board));
+		assertFalse(dummyQueen.canMove(3, 2, board));
+
+	}
+
 }
+
