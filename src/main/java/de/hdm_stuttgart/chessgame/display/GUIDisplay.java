ad2054c9467641d6ae001779f73d12bbeeed8d0a
Erik <erik@grosskopfonline.de>
2017-05-25 19:40:02 +0200

    startscreen test

--- src/main/java/de/hdm_stuttgart/chessgame/display/GUIDisplay.java
+++ src/main/java/de/hdm_stuttgart/chessgame/display/GUIDisplay.java
@@ -2,8 +2,11 @@ package de.hdm_stuttgart.chessgame.display;
 
 import de.hdm_stuttgart.chessgame.Game;
 import de.hdm_stuttgart.chessgame.pieces.*;
+import de.hdm_stuttgart.chessgame.GUIresources.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -39,8 +42,32 @@ public class GUIDisplay extends Application implements IDisplay
 	{
 		Game game = new Game(this);
 		
-		VBox root = new VBox();
+		Pane root = new Pane();
+		root.setId("pane");
 		Scene scene = new Scene(root, 500, 500);
+		
+        Button buttonStart = new Button("Start new Game");
+        buttonStart.autosize();
+        buttonStart.setLayoutX(250-75);
+        buttonStart.setLayoutY(180);
+        buttonStart.setId("button");
+        root.getChildren().add(buttonStart);
+        
+        Button buttonOptionen = new Button("Options");
+        buttonOptionen.autosize();
+        buttonOptionen.setLayoutX(250-75);
+        buttonOptionen.setLayoutY(230);
+        buttonOptionen.setId("button");
+        root.getChildren().add(buttonOptionen);
+        
+        Button buttonEnd = new Button("Quit ");
+        buttonEnd.autosize();
+        buttonEnd.setLayoutX(250-75);
+        buttonEnd.setLayoutY(280);
+        buttonEnd.setId("button");
+        root.getChildren().add(buttonEnd);
+		
+        scene.getStylesheets().addAll(this.getClass().getResource("/de/hdm_stuttgart/chessgame/GUIresources/style.css").toExternalForm());      
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
